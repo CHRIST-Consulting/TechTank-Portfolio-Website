79e3f1e93a099ddec6beba86babed7dff3db3c4d
Adharsh Jolly <ajcodesofficial+github@gmail.com>
2025-01-25 22:47:24 +0530

    Refactor Services component to use external data file for service details

---
+++ src/components/Services/data.js
@@ -0,0 +1,62 @@
+export const services = [
+  {
+    title: "Front End Development",
+    description:
+      "Our mission is to bridge the gap between technology and business success. We believe in creating value by providing transformative solutions that enhance efficiency, scalability, and innovation.",
+    imageUrls: {
+      baseUrl: "/images/services/frontend/",
+      images: ["fronend1.png", "fronend2.jpg", "fronend3.png", "fronend4.png"],
+    },
+  },
+  {
+    title: "Back End Development",
+    description:
+      "Our mission is to bridge the gap between technology and business success. We believe in creating value by providing transformative solutions that enhance efficiency, scalability, and innovation.",
+    imageUrls: {
+      baseUrl: "/images/services/backend/",
+      images: ["backend1.png", "backend2.png", "backend3.png", "backend4.png"],
+    },
+  },
+  {
+    title: "Full Stack Development",
+    description:
+      "Our mission is to bridge the gap between technology and business success. We believe in creating value by providing transformative solutions that enhance efficiency, scalability, and innovation.",
+    imageUrls: {
+      baseUrl: "/images/services/fullstack/",
+      images: [
+        "fullstack1.png",
+        "fullstack2.png",
+        "fullstack3.webp",
+        "fullstack4.webp",
+      ],
+    },
+  },
+  {
+    title: "Chatbots",
+    description:
+      "Our mission is to bridge the gap between technology and business success. We believe in creating value by providing transformative solutions that enhance efficiency, scalability, and innovation.",
+    imageUrls: {
+      baseUrl: "/images/services/chatbot/",
+      images: [
+        "chatbot1.webp",
+        "chatbot2.webp",
+        "chatbot3.jpg",
+        "chatbots4.webp",
+      ],
+    },
+  },
+  {
+    title: "Technical Training",
+    description:
+      "Our mission is to bridge the gap between technology and business success. We believe in creating value by providing transformative solutions that enhance efficiency, scalability, and innovation.",
+    imageUrls: {
+      baseUrl: "/images/services/technical/",
+      images: [
+        "techsupport1.jpg",
+        "techsupport2.png",
+        "techsupport3.png",
+        "techsupport4.png",
+      ],
+    },
+  },
+];

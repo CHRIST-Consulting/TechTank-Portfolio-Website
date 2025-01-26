76a0ef55b778c8c80b8faf2cd025bf834953e562
tarunkumar2005 <tarunkumar6258278@gmail.com>
2025-01-26 14:49:51 +0530

    Pushing the code of whole home page.

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Navbar } from './components/common/Navbar'
 import { Outlet } from 'react-router-dom';
+import Footer from './components/common/Footer';
 
-export default function () {
+export default function ({
+    activeTab,
+    setActiveTab,
+}) {
 
     return (
         <main className="min-h-screen w-full overflow-x-hidden bg-[#051650]">
-            <Navbar />
+            <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
             <div>
                 <Outlet />
             </div>
+            <Footer />
         </main>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,14 @@
-import { Navbar } from "@/components/Home/Navbar";
 import { Hero } from "@/components/Home/Hero";
 import BelowHero from "@/components/Home/BelowHero";
 import AboutUs from "@/components/Home/AboutUs";
 import OurClient from "@/components/Home/OurClient";
 import Testimonials from "@/components/Home/Testimonials";
-import Footer from "@/components/Home/Footer";
 
-export default function Home({ activeTab, setActiveTab }) {
+export default function Home() {
   return (
     <main className="min-h-screen w-full overflow-x-hidden bg-[#051650]">
       <div className="bg-gradient-to-b from-[#FFFFFF] via-[#678FFF] via-45% via-[#003AFF] via-[59.22%] to-[#051650] to-[78.1%] relative">
         <div className="relative z-10">
-          <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
           <Hero />
         </div>
       </div>
@@ -19,7 +16,6 @@ export default function Home({ activeTab, setActiveTab }) {
       <AboutUs />
       <OurClient />
       <Testimonials />
-      <Footer />
     </main>
   );
 }

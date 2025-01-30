6b36061eb3543b9520b6a53b9a1f2a37c9547364
tarunkumar2005 <tarunkumar6258278@gmail.com>
2025-01-30 22:03:39 +0530

    Changes in the Client Section

---
+++ src/components/Contact/TallyIframe.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+
+const TallyIframe = () => {
+  useEffect(() => {
+    const loadTallyEmbed = () => {
+      const d = document;
+      const w = "https://tally.so/widgets/embed.js";
+
+      const loadEmbed = () => {
+        d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((iframe) => {
+          iframe.src = iframe.dataset.tallySrc;
+        });
+      };
+
+      if (typeof window.Tally !== "undefined") {
+        window.Tally.loadEmbeds();
+      } else if (!d.querySelector(`script[src="${w}"]`)) {
+        const script = d.createElement("script");
+        script.src = w;
+        script.onload = loadEmbed;
+        script.onerror = loadEmbed;
+        d.body.appendChild(script);
+      }
+    };
+
+    loadTallyEmbed();
+  }, []);
+
+  return (
+    <iframe
+      data-tally-src="https://tally.so/embed/w49xrk?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+      loading="lazy"
+      width="100%"
+      height="576"
+      frameBorder="0"
+      marginHeight="0"
+      marginWidth="0"
+      title="Contact form"
+    ></iframe>
+  );
+};
+
+export default TallyIframe;

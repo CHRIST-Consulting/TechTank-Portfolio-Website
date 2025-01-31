370b13f76b3a5913126c2fa036591d5642da0b9a
Adharsh Jolly <ajcodesofficial+github@gmail.com>
2025-01-31 21:09:04 +0530

    Update gradient background in Contact page for improved visual consistency

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Contact() {
     return (
         <main>
-            <div className="bg-gradient-to-b from-white via-[#003AFF] via-[59.22%] to-[#0a2d9e] to-[78.1%]">
+            <div className="bg-gradient-to-b from-white via-[#003AFF] via-[35.22%] to-[#0a2d9e] to-[78.1%]">
                 <div className=" mx-auto flex-col items-center justify-center min-h-screen pt-32 pb-1">
                     <h1 className="text-6xl font-bold text-white mb-8 text-center">
                         Contact Us
